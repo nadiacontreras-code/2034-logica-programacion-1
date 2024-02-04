0b95e44cd6898212089dfc19316b81a2eeb3cef3
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2024-02-03 21:53:18 -0500

    Practicando logica de programacion I

---
+++ app.js
@@ -0,0 +1,62 @@
+
+//alert('¡Hola Mundo!');
+// numero secreto a adivinar
+/*let numeroSecreto = 6;
+let numeroDeUsuario = prompt("Me indicas un número por favor");
+
+console.log(numeroDeUsuario);
+
+if (numeroDeUsuario == numeroSecreto) {
+  alert("acertaste el número")
+} else {
+  alert("sigue intentando")
+}*/
+
+//---practica 01---
+/*let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');*/
+
+//---practica 02----
+/*alert('Bienvenidos al juego del número secreto');
+let eleccion = prompt('Elige un número entre 1 y 10');
+
+let numeroSecreto = 4;
+
+if (eleccion == numeroSecreto) {
+  alert('Adivinaste');
+}*/
+
+//----practica 03--- ERROR EN LA CONDICIONAL
+/*let contraseniaDelSistema = "contraseniaPrueba!";
+
+let contrasenia = prompt("Ingrese la contraseña del sistema:");
+
+if (contrasenia == contraseniaDelSistema) {
+  alert("Acceso al sistema garantizado");
+}*/
+
+//----------DESAFIOS ----------------------------------------------------------------
+
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+
+/*let nombre = "Luna";
+/let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+alert("Error! Completa todos los campos");
+
+let mensajeDeError = "Error! Completa todos los campos";
+
+alert(mensajeDeError);*/
+
+
+/*let nombre = prompt("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+
+if (edad >= 18) {
+  alert(nombre + " ¡Puedes obtener tu licencia de conducir!");
+} else {
+  alert(nombre + " No puedes tener licencia");
+}*/
+
